55c94ae727c5c248e20f84bb937427c52d486a0a
luklukass <104025385+luklukass@users.noreply.github.com>
2023-06-05 14:34:48 +0200

    with days

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,21 @@ class _ScrolledLayoutState extends State<ScrolledLayout> {
   DateTime? departureDate;
   int remainingDays = 0;
   int passedDays = 0;
+  int totalEarnedMoney = 0;
+  TextEditingController earnedMoneyController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    earnedMoneyController.addListener(computeEarnedMoney);
+  }
+
+  @override
+  void dispose() {
+    earnedMoneyController.removeListener(computeEarnedMoney);
+    earnedMoneyController.dispose();
+    super.dispose();
+  }
 
   Future<void> _selectArrivalDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
@@ -88,7 +103,7 @@ class _ScrolledLayoutState extends State<ScrolledLayout> {
         departureDate = picked;
         computePassedDays();
         computeRemainingDays();
-
+        computeEarnedMoney();
       });
     }
   }
@@ -102,13 +117,21 @@ class _ScrolledLayoutState extends State<ScrolledLayout> {
     }
   }
 
-
   void computePassedDays() {
     if (arrivalDate != null) {
       final difference = arrivalDate!.difference(DateTime.now()).abs();
       passedDays = difference.inDays;
     } else {
       passedDays = 0;
+    }
+  }
+
+  void computeEarnedMoney() {
+    if (arrivalDate != null && earnedMoneyController.text.isNotEmpty) {
+      int earnedMoneyPerDay = int.parse(earnedMoneyController.text);
+      totalEarnedMoney = earnedMoneyPerDay * passedDays;
+    } else {
+      totalEarnedMoney = 0;
     }
   }
 
@@ -138,7 +161,8 @@ class _ScrolledLayoutState extends State<ScrolledLayout> {
                           color: Colors.blue,
                           child: Center(
                             child: arrivalDate == null
-                                ? Text('Vyber datum příjezdu',
+                                ? Text(
+                              'Vyber datum příjezdu',
                               style: TextStyle(
                                 fontSize: 25, // Set the font size to 20
                                 fontWeight: FontWeight.bold, // Set the font weight to bold
@@ -146,7 +170,8 @@ class _ScrolledLayoutState extends State<ScrolledLayout> {
                               ),
                               textAlign: TextAlign.center,
                             )
-                                : Text('Příjezd: \n${DateFormat('dd/MM/yyyy').format(arrivalDate!)}',
+                                : Text(
+                              'Příjezd: \n${DateFormat('dd/MM/yyyy').format(arrivalDate!)}',
                               style: TextStyle(
                                 fontSize: 20, // Set the font size to 20
                                 fontWeight: FontWeight.bold, // Set the font weight to bold
@@ -173,7 +198,8 @@ class _ScrolledLayoutState extends State<ScrolledLayout> {
                             color: Colors.red,
                             child: Center(
                               child: departureDate == null
-                                  ? Text('Vyber datum odjezdu',
+                                  ? Text(
+                                'Vyber datum odjezdu',
                                 style: TextStyle(
                                   fontSize: 25, // Set the font size to 20
                                   fontWeight: FontWeight.bold, // Set the font weight to bold
@@ -181,7 +207,8 @@ class _ScrolledLayoutState extends State<ScrolledLayout> {
                                 ),
                                 textAlign: TextAlign.center,
                               )
-                                  : Text('Odjezd: \n${DateFormat('dd/MM/yyyy').format(departureDate!)}',
+                                  : Text(
+                                'Odjezd: \n${DateFormat('dd/MM/yyyy').format(departureDate!)}',
                                 style: TextStyle(
                                   fontSize: 20, // Set the font size to 20
                                   fontWeight: FontWeight.bold, // Set the font weight to bold
@@ -286,12 +313,49 @@ class _ScrolledLayoutState extends State<ScrolledLayout> {
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10), // Set circular corners
+                      borderRadius: BorderRadius.circular(10),
                       child: Container(
                         height: 150,
-                        color: Colors.purple,
-                        child: const Center(
-                          child: Text('Column 1, Row 3'),
+                        color: Colors.grey,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                'Částka za den',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              TextField(
+                                controller: earnedMoneyController,
+                                keyboardType: TextInputType.number,
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.black,
+                                ),
+                                textAlign: TextAlign.center,
+                                decoration: InputDecoration(
+                                  hintText: 'Zadej částku',
+                                  hintStyle: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black54,
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.grey,
+                                ),
+                                onChanged: (value) {
+                                  computeEarnedMoney();
+                                },
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -304,9 +368,33 @@ class _ScrolledLayoutState extends State<ScrolledLayout> {
                       borderRadius: BorderRadius.circular(10), // Set circular corners
                       child: Container(
                         height: 150,
-                        color: Colors.red,
-                        child: const Center(
-                          child: Text('Column 2, Row 3'),
+                        color: Colors.lightGreen,
+                        child: Center(
+                          child: RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: 'Vyděláno: \n',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: '$totalEarnedMoney',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
                     ),
@@ -314,42 +402,6 @@ class _ScrolledLayoutState extends State<ScrolledLayout> {
                 ),
               ],
             ),
-            SizedBox(height: 20), // Add a gap between rows
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10), // Set circular corners
-                      child: Container(
-                        height: 150,
-                        color: Colors.teal,
-                        child: const Center(
-                          child: Text('Column 1, Row 4'),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10), // Set circular corners
-                      child: Container(
-                        height: 150,
-                        color: Colors.pink,
-                        child: const Center(
-                          child: Text('Column 2, Row 4'),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20), // Add a gap at the bottom
           ],
         ),
       ),
