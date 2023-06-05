18875fa095fc49869620401b36df7dda1efa1160
luklukass <104025385+luklukass@users.noreply.github.com>
2023-06-05 11:37:41 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,287 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Motivator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black, // Set app bar background color to black
+        ),
+        scaffoldBackgroundColor: Colors.black26, // Set scaffold background color to light black
+      ),
+      home: const ScrolledLayout(),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate
+      ],
+      supportedLocales: [
+        Locale('en'),
+        Locale('cs')
+      ],
+    );
+  }
+}
+
+class ScrolledLayout extends StatefulWidget {
+  const ScrolledLayout({Key? key}) : super(key: key);
+
+  @override
+  _ScrolledLayoutState createState() => _ScrolledLayoutState();
+}
+
+class _ScrolledLayoutState extends State<ScrolledLayout> {
+  DateTime? arrivalDate;
+  DateTime? departureDate;
+
+  Future<void> _selectArrivalDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      locale: Locale('cs', 'CZ'), // Czech
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData.dark(), // Apply dark theme to the date picker
+          child: child!,
+        );
+      },
+    );
+
+    if (picked != null) {
+      setState(() {
+        arrivalDate = picked;
+      });
+    }
+  }
+
+  Future<void> _selectDepartureDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      locale: Locale('cs', 'CZ'), // Czech
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData.dark(), // Apply dark theme to the date picker
+          child: child!,
+        );
+      },
+    );
+
+    if (picked != null) {
+      setState(() {
+        departureDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Motivator'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20), // Add a gap at the top
+            GestureDetector(
+              onTap: () {
+                _selectArrivalDate(context);
+              },
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10), // Set circular corners
+                        child: Container(
+                          height: 150,
+                          color: Colors.blue,
+                          child: Center(
+                            child: arrivalDate == null
+                                ? Text('Vyber datum příjezdu',
+                              style: TextStyle(
+                                fontSize: 20, // Set the font size to 20
+                                fontWeight: FontWeight.bold, // Set the font weight to bold
+                                color: Colors.black, // Set the text color to white
+                              ),
+                              textAlign: TextAlign.center,
+                            )
+                                : Text('Příjezd: \n${DateFormat('dd/MM/yyyy').format(arrivalDate!)}',
+                              style: TextStyle(
+                                fontSize: 20, // Set the font size to 20
+                                fontWeight: FontWeight.bold, // Set the font weight to bold
+                                color: Colors.black, // Set the text color to white
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        _selectDepartureDate(context);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10), // Set circular corners
+                          child: Container(
+                            height: 150,
+                            color: Colors.red,
+                            child: Center(
+                              child: departureDate == null
+                                  ? Text('Vyber datum odjezdu',
+                                style: TextStyle(
+                                  fontSize: 20, // Set the font size to 20
+                                  fontWeight: FontWeight.bold, // Set the font weight to bold
+                                  color: Colors.black, // Set the text color to white
+                                ),
+                                textAlign: TextAlign.center,
+                              )
+                                  : Text('Odjezd: \n${DateFormat('dd/MM/yyyy').format(departureDate!)}',
+                                style: TextStyle(
+                                  fontSize: 20, // Set the font size to 20
+                                  fontWeight: FontWeight.bold, // Set the font weight to bold
+                                  color: Colors.black, // Set the text color to white
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20), // Add a gap between rows
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10), // Set circular corners
+                      child: Container(
+                        height: 150,
+                        color: Colors.orange,
+                        child: const Center(
+                          child: Text('Column 1, Row 2'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10), // Set circular corners
+                      child: Container(
+                        height: 150,
+                        color: Colors.yellow,
+                        child: const Center(
+                          child: Text('Column 2, Row 2'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20), // Add a gap between rows
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10), // Set circular corners
+                      child: Container(
+                        height: 150,
+                        color: Colors.purple,
+                        child: const Center(
+                          child: Text('Column 1, Row 3'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10), // Set circular corners
+                      child: Container(
+                        height: 150,
+                        color: Colors.red,
+                        child: const Center(
+                          child: Text('Column 2, Row 3'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20), // Add a gap between rows
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10), // Set circular corners
+                      child: Container(
+                        height: 150,
+                        color: Colors.teal,
+                        child: const Center(
+                          child: Text('Column 1, Row 4'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10), // Set circular corners
+                      child: Container(
+                        height: 150,
+                        color: Colors.pink,
+                        child: const Center(
+                          child: Text('Column 2, Row 4'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20), // Add a gap at the bottom
+          ],
+        ),
+      ),
+    );
+  }
+}
